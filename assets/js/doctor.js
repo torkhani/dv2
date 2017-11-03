719fe6e42b2a5dbf57034cf2164bfa353316a1d6
torkhani <torkhani.moetez@gmail.com>
2017-11-03 17:08:32 +0100

    add sticky view

--- assets/js/doctor.js
+++ assets/js/doctor.js
@@ -1,8 +1,8 @@
       function initMap() {
 
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 1,
-          center: {lat: -28.024, lng: 140.887}
+          zoom: 8,
+          center: {lat: 36.7948008, lng: 10.0031931 }
         });
 
         // Create an array of alphabetical characters used to label the markers.
@@ -25,44 +25,15 @@
       }
 	  
       var locations = [
-        {lat: -31.563910, lng: 147.154312},
-        {lat: -33.718234, lng: 150.363181},
-        {lat: -33.727111, lng: 150.371124},
-        {lat: -33.848588, lng: 151.209834},
-        {lat: -33.851702, lng: 151.216968},
-        {lat: -34.671264, lng: 150.863657},
-        {lat: -35.304724, lng: 148.662905},
-        {lat: -36.817685, lng: 175.699196},
-        {lat: -36.828611, lng: 175.790222},
-        {lat: -37.750000, lng: 145.116667},
-        {lat: -37.759859, lng: 145.128708},
-        {lat: -37.765015, lng: 145.133858},
-        {lat: -37.770104, lng: 145.143299},
-        {lat: -37.773700, lng: 145.145187},
-        {lat: -37.774785, lng: 145.137978},
-        {lat: -37.819616, lng: 144.968119},
-        {lat: -38.330766, lng: 144.695692},
-        {lat: -39.927193, lng: 175.053218},
-        {lat: -41.330162, lng: 174.865694},
-        {lat: -42.734358, lng: 147.439506},
-        {lat: -42.734358, lng: 147.501315},
-        {lat: -42.735258, lng: 147.438000},
-        {lat: -43.999792, lng: 170.463352}
+        {lat: 36.7948008, lng: 10.0031931},
+        {lat: 36.825384, lng: 10.1538094},
+        {lat: 36.825587, lng: 10.151718},
+        {lat: 36.8277641, lng: 10.1571468},
+        {lat: 36.8435906, lng: 10.1902912},
+        {lat: 36.4187378, lng: 10.4737883},
+        {lat: 36.3759574, lng: 10.5253314},
       ]
 	  
-	  
-	  
-	$(window).scroll(function() {    
-		var scroll = $(window).scrollTop();
-        var resultHeight = ($('.search-list').height() / 2);
 
-        if (scroll >= 66 && scroll < resultHeight) {
-            $(".search-form").addClass("stuck");
-            $(".search-map-wrapper .search-map").addClass("stuck-map");
-            $(".search-map-wrapper .search-map").width($('.search-map-wrapper').width());
-        } else {
 
-            $(".search-map-wrapper .search-map").css('top', -scroll + "px");
-            $(".search-form").removeClass("stuck");
-        }
-	}); //missing );
+$("#sidebar, #search-header").stick_in_parent({recalc_every: 1});
